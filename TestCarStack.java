49df23d11ed9390a660b95385478d60f3b2fe75b
Priyantha Buddhika <36040643+priyanthabuddhika@users.noreply.github.com>
2021-01-12 21:42:07 +0530

    Update TestCarStack.java

--- TestCarStack.java
+++ TestCarStack.java
@@ -1,5 +1,5 @@
 
-package testcarstack;
+package linkedlist;
 
 class Car
 {
@@ -110,15 +110,3 @@ class Stacks
 }
 
 
-public class TestCarStack {
-    public static void main(String[] args) {
-         Stacks carstack =new Stacks();
-         Car car1=new Car(698,"Toyota");
-         Car car2=new Car(5698,"Suzuki");
-         carstack.push(car1);
-         carstack.push(car2);
-         carstack.pop();
-         carstack.peek();
-    }
-
-}
